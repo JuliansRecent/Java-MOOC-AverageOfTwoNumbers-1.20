8f5b6d9571cf1f3b469617b58f578424736243a7
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 18:08:21 -0800

    Module complete. Prompt the user for two numbers and then canlculate the average and turn the integers into a double.

--- src/main/java/AverageOfTwoNumbers.java
+++ src/main/java/AverageOfTwoNumbers.java
@@ -6,7 +6,15 @@ public class AverageOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        // Write your program here;
+        // Prompt the user for two numbers
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        // Add the two inputted numbers and divide them by two to get the average
+        double average = (1.0 * (firstNumber + secondNumber)) / 2;
+        // Print the average to the console
+        System.out.println("The average is " + average);
     }
 }
